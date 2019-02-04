b1a290b566c4d40c24396fa8f588efdb7d70f18f
SIY1121 <sota.0512.0213@gmail.com>
2019-02-04 22:40:06 +0900

    wip

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ describe('Module', () => {
             assert.equal(index.add(1, 2), 3);
         });
         it ('index.jsが正しく機能しているか2', () => {
-            assert.equal(index.add(1, 3), 3);
+            assert.equal(index.add(1, 3), 4);
         });
     });
 })
