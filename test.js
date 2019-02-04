ba41933012c353ab1263e23aaf671bb892cf060c
SIY1121 <sota.0512.0213@gmail.com>
2019-02-04 22:39:07 +0900

    変更

--- test.js
+++ test.js
@@ -6,5 +6,8 @@ describe('Module', () => {
         it ('index.jsが正しく機能しているか', () => {
             assert.equal(index.add(1, 2), 3);
         });
+        it ('index.jsが正しく機能しているか2', () => {
+            assert.equal(index.add(1, 3), 3);
+        });
     });
 })
